c435bd41a8a4cf60f0b29391247fc6a4d7370db1
petik.balazs <petik.balazs@tigra.hu>
2021-11-10 16:54:32 +0100

    infra versions

---
+++ install_infra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PIP3 install ansible==2.10.6
+pip3 install  molecule==3.2.3
+pip3 install molecule_docker==0.2.4
+PIP3 install pytest_testinfra==6.1.0
+pip3 install estinfra==6.1.0
+pip3 install yamllint
+pip3 install flake8
+
+
