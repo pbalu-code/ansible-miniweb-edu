7ac71b992dcbb769c3351aa246b7164c3f38ddde
petik.balazs <petik.balazs@tigra.hu>
2021-11-10 18:05:29 +0100

    new name

--- install_infra.sh
+++ install_infra.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PIP3 install ansible==2.10.6
-pip3 install  molecule==3.2.3
+pip3 install ansible==2.10.6
+pip3 install molecule==3.2.3
 pip3 install molecule_docker==0.2.4
-PIP3 install pytest_testinfra==6.1.0
-pip3 install estinfra==6.1.0
+pip3 install pytest_testinfra==6.1.0
+pip3 install testinfra==6.1.0
 pip3 install yamllint
 pip3 install flake8
 
